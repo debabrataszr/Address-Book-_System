8808fc8df9a5e186a63dc6c666959ec12697b8c2
debabrata-97 <debabrataszr@gmail.com>
2023-03-26 23:36:40 +0530

    Added : Code to edit a contact in AddressBook

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,4 +1,3 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class AddressBook {
@@ -12,16 +11,20 @@ public class AddressBook {
     public static void askToUser() {
 
         int res = 0;
-        while (res != 2) {
+        while (res != 3) {
             System.out.println("Select an option : ");
             System.out.println("\n1. Add Contact in Address book");
-            System.out.println("\n2. Exit from the Address book");
+            System.out.println("\n2. Edit Contact in Address book");
+            System.out.println("\n3. Exit from the Address book");
             res = sc.nextInt();
             switch (res) {
                 case 1:
                     addTheContact();
                     break;
                 case 2:
+                    editTheContact();
+                    break;
+                case 3:
                     exitFromAddressbook();
                     break;
                 default:
@@ -29,6 +32,7 @@ public class AddressBook {
             }
         }
     }
+
     public static void addTheContact() {
         System.out.println("Enter Firstname :");
         String firstname = sc.next();
@@ -48,8 +52,32 @@ public class AddressBook {
         String email = sc.next();
 
         System.out.println("DETAILS OF PERSON IS ADDED");
-        System.out.println(firstname +" " +lastname +" "+address+" "+city+" "+state+" "+zip+" "+phoneno+" "+email);
+        System.out.println(firstname + " " + lastname + " " + address + " " + city + " " + state + " " + zip + " " + phoneno + " " + email);
         System.out.println("**************************************** CONTACT ADDED SUCCESSFULLY *****************************************");
+    }
+
+    public static void editTheContact() {
+
+        System.out.println("Enter Firstname to Edit :");
+        String firstname = sc.next();
+        System.out.println("Enter Lastname to Edit : +firstname");
+        String lastname = sc.next();
+        System.out.println("Enter Address to Edit : ");
+        String address = sc.next();
+        System.out.println("Enter City to Edit : ");
+        String city = sc.next();
+        System.out.println("Enter State to Edit  : ");
+        String state = sc.next();
+        System.out.println("Enter zipcode to Edit : ");
+        int zip = sc.nextInt();
+        System.out.println("Enter Phone no. to Edit : ");
+        long phoneno = sc.nextLong();
+        System.out.println("Enter email to Edit : ");
+        String email = sc.next();
+
+        System.out.println("DETAILS OF PERSON IS EDITED");
+        System.out.println(firstname + " " + lastname + " " + address + " " + city + " " + state + " " + zip + " " + phoneno + " " + email);
+        System.out.println("**************************************** CONTACT EDITED SUCCESSFULLY *****************************************");
     }
 
     public static void exitFromAddressbook() {
